85f7873da275dd2052b7694a7a6700edadc5ca60
Hariprasath006 <harinataraj2005@gmail.com>
2026-03-15 22:14:56 +0530

    Update AdminLogin.js

--- frontend/src/pages/AdminLogin.js
+++ frontend/src/pages/AdminLogin.js
@@ -20,7 +20,7 @@ function AdminLogin() {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/users/login", {
+      const res = await axios.post("https://nexgen-yg2a.onrender.com/api/users/login", {
         email,
         password
       });
