471c76a92cb45196167bfee3ffd556b24debd26c
Hariprasath <harinataraj2005@gmail.com>
2026-03-22 09:37:17 +0530

    fix(auth): Add missing toast import to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import HomePage from "./pages/HomePage";
